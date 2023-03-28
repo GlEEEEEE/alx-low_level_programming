e49dad4d5f2797d891d2692fbb0349d0d324b8dd
Bfocus12 <augustusjoys91@gmail.com>
2023-03-28 15:17:51 -0700

    prints string in reverse followed by new line

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,10 +3,8 @@
  * rev_string - reverses a string
  * _putchar - print each character
  * @s: char to check
- *
- * Description: This will reverse a string
- * Return: 0 is success
  */
+
 void rev_string(char *s)
 {
 	int a = 0, b, c;
