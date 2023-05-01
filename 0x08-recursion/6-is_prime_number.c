d6367569a889d4768d8ee443709742b9d1d46709
Bfocus12 <augustusjoys91@gmail.com>
2023-05-01 12:56:50 +0000

    function returns 1 if input integer is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
-* is_prime_number - checks if a number is prime
-*
-* @n: the number to check
-*
-* Return: 1 if n is prime, 0 otherwise
-*/
-
-int is_prime_number(int n)
-{
-if (n < 2) /* 0 and 1 are not prime */
-return (0);
-return (check_prime(n, 2));
-}
-
-/**
 * check_prime - checks if a number is prime recursively
 *
 * @n: check number
@@ -32,4 +17,19 @@ else if (divisor >= n / 2) /* no factors */
 return (1);
 else
 return (check_prime(n, divisor + 1));
+}
+
+/**
+* is_prime_number - checks if a number is prime
+*
+* @n: the number to check
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+if (n < 2) /* 0 and 1 are not prime */
+return (0);
+return (check_prime(n, 2));
 }
