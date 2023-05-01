7724ef02a2e742bda187f1fa0450784e33eb881d
Bfocus12 <augustusjoys91@gmail.com>
2023-05-01 17:17:47 +0000

    function that returns a pointer to a newly allocated space in memory con string and param

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 /**
+* _strlen - returns the length of a string
+* @s: the string to measure
+*
+* Return: the length of the string
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+
+/**
 * _strdup - returns a pointer to a newly allocated space in memory
 * containing a copy of the string given as parameter
 * @str: string to duplicate
