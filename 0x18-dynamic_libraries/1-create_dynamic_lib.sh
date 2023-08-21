7addde201e1ba8a48acc3cd75e3310c5b20a002c
Bfocus12 <augustusjoys91@gmail.com>
2023-08-21 21:14:11 +0000

    script for dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -o liball.so *.c
 export LD_LIBRARY_PATH.:$LD_LIBRARY_PATH
-
